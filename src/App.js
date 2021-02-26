c1246e88304e94b4cb4ad4838bab5abdf616325e
tgbf2674 <tgbf2020@163.com>
2021-02-26 14:16:56 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React from "react";
+import React, {useEffect, useState } from "react";
 
-// class App extends React.Component{
-//   constructor(props){
-//     super(props)
-//     this.state = {n:1}
-//   }
-//   onClick=()=>{this.setState(state=>({n: state.n+1}))}
-  
-//   render (){
-//     return(
-//       <div>
-//       {this.state.n}
-//       <button onClick={this.onClick}>+1</button>
-//       </div>
-//     )
-// }
-// }
+
 
 const App = (props)=>{
-  const [n,setN] = React.useState(0)
+  const [childVisible,setChildVisible] = useState(true)
+  const show = ()=>{
+    setChildVisible(true)
+  }
+  const hide =()=>{
+    setChildVisible(false)
+  }
+
     return(
       <div>
-        {n}
-        <button onClick={()=>setN(n+1)}>+1</button>
+        {childVisible ? <button onClick={hide}>hide</button> : <button onClick={show}>show</button>} 
+        {childVisible? <Child />: null}
       </div>
     )
+}
+
+const Child=(props)=>{
+  useEffect(()=>{
+    return ()=>{
+      console.log('disapper')
+    }
+  })
+  return(
+    <div>
+      child
+    </div>
+  )
 }
 
 export default App;
